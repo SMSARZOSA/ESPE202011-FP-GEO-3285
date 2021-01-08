4d34610d14d4d5f890630e47254c6ae998ca1f2e
SMSARZOSA <shirley55sarzosa55@gmail.com>
2021-01-08 18:33:16 -0500

    workshop 01

---
+++ workshops/unit1/WS01/HelloWorld.java
@@ -0,0 +1,12 @@
+// Shirley Sarzosa Hello World
+
+/*
+some lines of comments
+can be written in this way
+thanks
+*/
+class HelloWorld{
+public static void main(strings[] args){
+System.out.println("Hello World from Shirley Sarzosa");
+}
+}
---
+++ workshops/unit1/WS01/Helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+//printf() displays information
+printf("Hellow world from/t/"Shirley Sarzosa!=)");
+printf("/n the grade is %d /n", grade);
+return 0;
+}
