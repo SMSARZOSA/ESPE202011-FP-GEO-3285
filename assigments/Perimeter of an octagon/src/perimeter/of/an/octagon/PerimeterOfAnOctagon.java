41b7fac2919333f6c978e75a599297b3c9753216
SMSARZOSA <shirley55sarzosa55@gmail.com>
2021-01-10 16:17:34 -0500

    assigments 01 perimeter octagon

---
+++ assigments/Perimeter of an octagon/src/perimeter/of/an/octagon/PerimeterOfAnOctagon.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perimeter.of.an.octagon;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Shirley
+ */
+public class PerimeterOfAnOctagon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     int side1;
+     int perimeter;
+     
+     float side2;
+     float perimeter2;
+     
+     short side3;
+     short perimeter3;
+     
+                       
+    side1= 20;
+    side2= (float) 12.2;
+    side3= (short) 1000 ;
+    
+    perimeter=(int) (side1*8);
+    perimeter2=(float) (side2*8);
+    perimeter3= (short) (side3*8);
+    
+    
+    
+    System.out.println("the perimeter of " + side1 + " * " + 8 + " is equal to " + perimeter);
+    System.out.println("the perimeter of " + side2 + " * " + 8 + " is equal to " + perimeter2);
+    System.out.println("the perimeter of " + side3 + " * " + 8 + " is equal to " + perimeter3);
+  
+ 
+     
+     
+     
+
+     
+     
+     
+             
+         
+         
+         
+                 
+        
+    }
+    
+}
